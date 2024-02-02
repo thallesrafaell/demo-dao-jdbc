6c183ce0c822d73915053bbb8edc3547a992a133
Thalles Rafael <thallesrafaell@outlook.com>
2024-02-02 17:11:44 -0300

    Department Update implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -18,14 +18,20 @@ public class Program2 {
         System.out.println(departmentFound);
         System.out.println();
 
-        System.out.println("===== Test 1 : Department findById =====");
+        System.out.println("===== Test 2 : Department findById =====");
         List<Department> departmentsList = departmentDao.findAll();
         departmentsList.stream().forEach(department -> System.out.println(department));
         System.out.println();
-        
-        System.out.println("===== Test 1 : Department Delete =====");
+
+        System.out.println("===== Test 3 : Department Delete =====");
         departmentDao.deleteById(6);
         System.out.println("Deleted completed!");
         System.out.println();
+
+        System.out.println("===== Test 4 : Department Update =====");
+        Department department =new Department(4, "Computer");
+        departmentDao.update(department);
+        departmentFound = departmentDao.findById(4);
+        System.out.println(departmentFound);
     }
 }
